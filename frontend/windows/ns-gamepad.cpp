a48f68947e0a89b9152dfaff33be3850447c6ec3
Dycool <65285981+Dycool@users.noreply.github.com>
2026-06-01 22:37:34 +0100

    Update ns-gamepad.cpp

--- frontend/windows/ns-gamepad.cpp
+++ frontend/windows/ns-gamepad.cpp
@@ -180,7 +180,7 @@ ns::HIDReport map_xinput_to_switch(const XINPUT_GAMEPAD& pad) {
 int main(int argc, char** argv) {
     // Validate command-line arguments
     if (argc < 2) {
-        std::cerr << "Usage: gamepad.exe <RASPBERRY PI IP>\n";
+        std::cerr << "Usage: " << argv[0] << " <RASPBERRY PI IP>\n";
         return 1;
     }
 
