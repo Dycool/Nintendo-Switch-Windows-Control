e488c0b65734c6456ac380ca0e0c97bf04b5f577
Diogo Enes <ist1113618@tecnico.ulisboa.pt>
2026-06-03 14:40:28 +0100

    yes

--- client/windows/build.bat
+++ client/windows/build.bat
@@ -4,18 +4,18 @@
 echo === Building console client ===
 where cl >nul 2>&1
 if %ERRORLEVEL% == 0 (
-    cl /std:c++17 /O2 /EHsc /W3 ns-gamepad.cpp /link ws2_32.lib xinput.lib /out:gamepad.exe
+    cl /std:c++17 /O2 /EHsc /W3 ns-gamepad.cpp /link ws2_32.lib xinput.lib winmm.lib /out:gamepad.exe
     goto :gui
 )
 
 where g++ >nul 2>&1
 if %ERRORLEVEL% == 0 (
-    g++ -std=c++17 -O2 -Wall ns-gamepad.cpp -o gamepad.exe -static -lws2_32 -lxinput
+    g++ -std=c++17 -O2 -Wall ns-gamepad.cpp -o gamepad.exe -static -lws2_32 -lxinput -lwinmm
     goto :gui
 )
 
 if exist "C:\msys64\ucrt64\bin\g++.exe" (
-    C:\msys64\ucrt64\bin\g++.exe -std=c++17 -O2 -Wall ns-gamepad.cpp -o gamepad.exe -static -lws2_32 -lxinput
+    C:\msys64\ucrt64\bin\g++.exe -std=c++17 -O2 -Wall ns-gamepad.cpp -o gamepad.exe -static -lws2_32 -lxinput -lwinmm
 )
 
 :gui
